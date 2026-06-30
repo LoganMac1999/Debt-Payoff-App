f5396709ff8cea6ce9370c80cc6e4c2f3cd89296
Logan MacPhail <logan@example.com>
2026-06-30 23:36:13 +0000

    Fix session persistence on iOS PWA

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const SUPABASE_URL = 'https://roqlxsfaleuwcfwordfk.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_0MvSNh-6L6v3q2iV2cxWiw_ect60eLk'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    persistSession: true,
+    storage: window.localStorage,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  }
+})
